849fcc05af4cd4b2a85672880bad36d6484655c8
Damho Lee <ydh4745@gmail.com>
2023-01-09 20:57:16 +0900

    Tetris.java add comment

--- com/zetcode/Tetris.java
+++ com/zetcode/Tetris.java
@@ -22,17 +22,17 @@ public class Tetris extends JFrame {
 
     private void initUI() {
 
-        statusbar = new JLabel(" 0");
-        add(statusbar, BorderLayout.SOUTH);
+        statusbar = new JLabel(" 0"); //점수
+        add(statusbar, BorderLayout.SOUTH); //남쪽에 배치
 
         var board = new Board(this);
         add(board);
         board.start();
 
-        setTitle("Tetris");
-        setSize(200, 400);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+        setTitle("Tetris");                //title명
+        setSize(200, 400);         //사이즈 조절
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //x누르면 작동 끝
+        setLocationRelativeTo(null);          //화면의 가운데 테트리스 게임 출력
     }
 
     JLabel getStatusBar() {
@@ -42,10 +42,11 @@ public class Tetris extends JFrame {
 
     public static void main(String[] args) {
 
-        EventQueue.invokeLater(() -> {
+        EventQueue.invokeLater(() -> {//이벤트 디스패칭 쓰레드에 의해 Runnalbe.run()을 실행한다. 
+                                      //이 메소드를 호출하면 이벤트 큐에 runnable.run()의 내용을 넣고 바로 리턴한다.
 
             var game = new Tetris();
-            game.setVisible(true);
+            game.setVisible(true); 
         });
     }
 }
